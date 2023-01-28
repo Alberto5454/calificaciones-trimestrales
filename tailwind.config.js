b4f0ec3b2bbc20ddbcd3f3d373ef73d2430e2a1f
alberto <est.jobregonb0201@uml.edu.ni>
2023-01-28 14:54:39 -0600

    actualizacion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/css/**/*.css',
+    './index.html',
+  ],
   theme: {
     extend: {},
   },
